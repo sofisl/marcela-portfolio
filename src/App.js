bf5cbb96e639978db4badff5e8428b3b5b079f68
Sofia Leon <sofialeon@google.com>
2020-06-16 23:45:44 -0700

    feat: stub out beginning of components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path="/" component {Home} />
+            <Route path="/about" component {About} />
+            <Route path="/illustrations" component {Illustrations} />
+            <Route path="/curriculum-designs" component {CurriculumDesigns} />
+            <Route path="/other-ideas" component {OtherIdeas} />
+            <Route path="/contact" component {Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
